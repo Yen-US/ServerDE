daefe5bbcb912f2dec56b2744a5b3476e264b6c8
yenus <yen21000@gmail.com>
2019-08-18 14:14:45 -0600

    Se sube el codigo de listas enlazadas

---
+++ cmake-build-debug/CMakeFiles/ServerDE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerDE.dir/main.cpp.o"
+  "ServerDE.pdb"
+  "ServerDE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
